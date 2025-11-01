6396652aa2fb753f2024ac93e7e31be6ef112138
mini0192 <parkgw9071@gmail.com>
2025-11-01 17:54:32 +0900

    Feat: v1_complate

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
-export * as animation from "./animation";
-export * as basicUi from "./basic-ui";
-export * as section from "./section";
-export * as ui from "./ui";
-
 export * from "./animation";
 export * from "./basic-ui";
 export * from "./section";
 export * from "./ui";
+
+export * from "./miniComponentConfig";
