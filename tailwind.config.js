6e1c12efdb49dad99468283cde0d64a0f6e66033
mini0192 <parkgw9071@gmail.com>
2025-11-01 20:42:31 +0900

    Feat: please do it

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  safelist: [{ pattern: /.*/ }],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
